36f94e51602914506d85a44fb887a963b31f0665
n0thing <78114574+imn0th1ng@users.noreply.github.com>
2022-03-07 23:02:11 +0300

    Update server.lua

--- AntiDump/server.lua
+++ AntiDump/server.lua
@@ -1,6 +1,12 @@
 local name = GetCurrentResourceName()
+local antidump ={}
 
 RegisterServerEvent("sendclient-"..name, function()
     local code = LoadResourceFile(name, "test.lua")
-    TriggerClientEvent("getclient-"..name, source, code)
+    if antidump[source] == nil then
+            antidump[source] = true
+        TriggerClientEvent("getclient-"..name, source, code)
+    else
+        DropPlayer(source, "?")
+    end
 end)
